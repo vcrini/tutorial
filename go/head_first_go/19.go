9b1686155f1e3b7a4cfec6726e6a73ff2cf756e9
valerio <vcrini@gmail.com>
2023-01-02 15:55:03 +0100

    adding e.s. 19

---
+++ go/head_first_go/19.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  quantity := 4
+  length, width := 1.2, 2.4
+  customerName := "Valerio Crini"
+  fmt.Println(customerName)
+  fmt.Println("ha ordinato ", quantity)
+  fmt.Println("ogniuno con un perimetro di ",length*width)
+}
