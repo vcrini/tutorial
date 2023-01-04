bc957797d72400b2d9835015187d6010a82193ec
vcrini <vcrini@mini.mini>
2023-01-04 18:32:32 +0100

    adding condition

---
+++ go/head_first_go/1752.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "log"
+  "os"
+)
+func main() {
+  fmt.Println("Inserisci un grado:")
+  reader := bufio.NewReader(os.Stdin)
+  input, err  := reader.ReadString('\n')
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(input)
+}
