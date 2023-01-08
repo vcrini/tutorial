860388eae8ade599f69e29782dcadcc17a996b21
vcrini <vcrini@mini.mini>
2023-01-08 12:03:31 +0100

    real random number

--- go/head_first_go/guess.go
+++ go/head_first_go/guess.go
@@ -3,8 +3,11 @@ package main
 import (
   "fmt"
   "math/rand"
+  "time"
 )
 func main() {
+  seconds := time.Now().Unix()
+  rand.Seed(seconds)
   target := rand.Intn(100)+1
   fmt.Println(target)
 }
