ca7214a7ae75c662be9635a75be0f41e962038c8
vcrini <vcrini@mini.mini>
2023-01-09 12:44:13 +0100

    added functions

--- go/head_first_go/2525.go
+++ go/head_first_go/2525.go
@@ -3,15 +3,16 @@ package main
 import (
 	"fmt"
 )
-
+func painNeeded(width float64, height float64) {
+  area := width*height
+	fmt.Printf("%.2f\n", area/10.0)
+}
 func main() {
-	var width, height, area float64
+	var width, height float64
 	width = 4.2
 	height = 3.0
-	area = width * height
-	fmt.Printf("%.2f\n", area/10.0)
+  painNeeded(width,height)
 	width = 5.2
 	height = 3.5
-	area = width * height
-	fmt.Printf("%.2f\n", area/10.0)
+  painNeeded(width,height)
 }
