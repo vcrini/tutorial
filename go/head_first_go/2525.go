72001626b23487dd54be9757ba6db3246c8a2eba
vcrini <vcrini@mini.mini>
2023-01-09 12:52:36 +0100

    returning values in a function

--- go/head_first_go/2525.go
+++ go/head_first_go/2525.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 )
-func painNeeded(width float64, height float64) {
+func painNeeded(width float64, height float64) float64 {
   area := width*height
-	fmt.Printf("%.2f\n", area/10.0)
+  return area/10.0
 }
 func main() {
 	var width, height float64
 	width = 4.2
 	height = 3.0
   painNeeded(width,height)
+	fmt.Printf("%.2f\n", painNeeded(width,height))
 	width = 5.2
 	height = 3.5
-  painNeeded(width,height)
+	fmt.Printf("%.2f\n", painNeeded(width,height))
 }
