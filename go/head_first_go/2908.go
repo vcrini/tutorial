f559e0d896b1d8654bbf9be4a96e75158e5b576e
vcrini <vcrini@mini.mini>
2023-01-09 16:57:09 +0100

    adding more variables

--- go/head_first_go/2908.go
+++ go/head_first_go/2908.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	var myInt int
+  var myIntPointer *int
 	fmt.Println(reflect.TypeOf(myInt))
+	fmt.Println(reflect.TypeOf(myIntPointer))
+	fmt.Println(&myInt)
+	fmt.Println(&myIntPointer)
+  myIntPointer=&myInt
+	fmt.Println(&myInt)
+	fmt.Println(&myIntPointer)
 	var myFloat float64
 	fmt.Println(reflect.TypeOf(myFloat))
 	var myBool bool
