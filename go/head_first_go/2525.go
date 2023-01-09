6e76f1137741a2c2a4819a3f218a9f9c1dc34574
vcrini <vcrini@mini.mini>
2023-01-09 15:39:05 +0100

    added error conditions

--- go/head_first_go/2525.go
+++ go/head_first_go/2525.go
@@ -3,17 +3,37 @@ package main
 import (
 	"fmt"
 )
-func painNeeded(width float64, height float64) float64 {
-  area := width*height
-  return area/10.0
+
+func paintNeeded(width float64, height float64) (float64, error) {
+	area := width * height
+	if width < 0 {
+		return 0, fmt.Errorf("width %.2f can't be negative: ", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("height %.2f can't be negative: ", height)
+	}
+	return area / 10.0, nil
 }
 func main() {
 	var width, height float64
 	width = 4.2
 	height = 3.0
-  painNeeded(width,height)
-	fmt.Printf("%.2f\n", painNeeded(width,height))
-	width = 5.2
+	amount, err := paintNeeded(width, height)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("area is %.2f\n", amount)
+	}
+	// fmt.Printf("%.2f\n", painNeeded(width,height))
+	// width = 5.2
+	// height = 3.5
+	// fmt.Printf("%.2f\n", painNeeded(width,height))
+	width = -1.0
 	height = 3.5
-	fmt.Printf("%.2f\n", painNeeded(width,height))
+	amount, err = paintNeeded(width, height)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("area is %.2f\n", amount)
+	}
 }
