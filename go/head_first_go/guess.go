c10d9a3f4e42ee13442616f28b092f937a298e4a
vcrini <vcrini@mini.mini>
2023-01-09 10:19:07 +0100

    guess game finished

--- go/head_first_go/guess.go
+++ go/head_first_go/guess.go
@@ -1,39 +1,44 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "log"
-  "math/rand"
-  "os"
-  "strconv"
-  "strings"
-  "time"
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
-func main() {
-  seconds := time.Now().Unix()
-  rand.Seed(seconds)
-  target := rand.Intn(100)+1
-  fmt.Println(target)
 
-  reader :=bufio.NewReader(os.Stdin)
-  fmt.Println("Fai un tentativo di indovinare")
-  input, err := reader.ReadString('\n')
-  if err != nil {
-    log.Fatal(err)
-  }
-  input = strings.TrimSpace(input)
-  guess, err := strconv.Atoi(input)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if guess>target {
-    fmt.Println("Alto")
-  }
-  if guess < target {
-    fmt.Println("Basso")
-  }
-  if guess == target {
-    fmt.Println("Indovinato")
-  }
-  }
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println(target)
+
+	for guesses := 0; guesses < 3; guesses++ {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Fai un tentativo di indovinare")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if guess > target {
+			fmt.Println("Alto")
+		}
+		if guess < target {
+			fmt.Println("Basso")
+		}
+		if guess == target {
+			fmt.Println("Indovinato")
+      break
+		}
+
+	}
+}
