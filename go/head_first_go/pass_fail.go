717aaa955b77ead8b28b29b374c61f87c3defb8a
vcrini <vcrini@mini.mini>
2023-01-12 19:02:52 +0100

    adding 'repetitive' grade program

---
+++ go/head_first_go/pass_fail.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+func main() {
+	fmt.Print("Inserisci un valore: ")
+	grade, err := getFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var status string
+	if grade >= 60 {
+		status = "passing"
+	} else {
+		status = "falling"
+	}
+	fmt.Println("Un voto di ", grade, "è ", status)
+}
