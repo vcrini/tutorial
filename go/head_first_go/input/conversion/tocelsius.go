25308efc787355b67ecf6f0cf3bde1b672ab6a42
vcrini <vcrini@mini.mini>
2023-01-16 21:49:05 +0100

    using constant

--- go/head_first_go/input/conversion/tocelsius.go
+++ go/head_first_go/input/conversion/tocelsius.go
@@ -13,6 +13,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	celsius := (fahrenheit - 32) * 5
+  const base = 32
+	celsius := (fahrenheit - base) * 5
 	fmt.Printf("%0.2f gradi celsius\n", celsius)
 }
