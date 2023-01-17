4b27e475becc7f60d2d780d08f26dc2ce57a6f20
vcrini <vcrini@mini.mini>
2023-01-17 20:16:01 +0100

    advanced loop

--- go/head_first_go/array.go
+++ go/head_first_go/array.go
@@ -1,14 +1,21 @@
 package main
 
-// just to show how to use arrays
 import "fmt"
 
 func main() {
 
+  // just to show how to use arrays
 	var element [3]int
 	element[0] = 2
 	for i := 0; i < 3; i++ {
 		fmt.Printf("il %d numero è  %d\n", i ,element[i])
 	}
+  element2:=[3]int{1,2,3}
+  //reading from file
+  for _,v := range element2{
+    fmt.Printf("%d\n",v)
+    
+  }
+
 
 }
