f2f4abf85245044e0454070d5c290f8b3e01cce4
vcrini <vcrini@mini.mini>
2023-01-20 21:18:59 +0100

    reading from file

--- go/head_first_go/array.go
+++ go/head_first_go/array.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -21,5 +25,36 @@ func main() {
 	n := float64(len(element2))
 
 	fmt.Printf("la media di %v è %0.2f\n", element2, sum/n)
+	// reading from file
+	fmt.Println("il contenuto del file è")
+	file, err := os.Open("array.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	var i int
+	var element3 [3]float64
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		element3[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		i += 1
+	}
+	err = file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if scanner.Err() != nil {
+		log.Fatal(err)
+	}
+  fmt.Println("La media dei valori contenuti nel file è")
+  sum=0
+	for _, v := range element3 {
+		sum += v
+	}
+	n = float64(len(element3))
 
+	fmt.Printf("la media di %v è %0.2f\n", element3, sum/n)
 }
