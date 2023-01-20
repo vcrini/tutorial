e8d146d4917a8539c9b2f2039a5510e3a542c406
vcrini <vcrini@mini.mini>
2023-01-20 20:44:09 +0100

    calcolando la media

--- go/head_first_go/array.go
+++ go/head_first_go/array.go
@@ -1,21 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-  // just to show how to use arrays
+	// just to show how to use arrays
 	var element [3]int
 	element[0] = 2
 	for i := 0; i < 3; i++ {
-		fmt.Printf("il %d numero è  %d\n", i ,element[i])
+		fmt.Printf("il %d numero è  %d\n", i, element[i])
 	}
-  element2:=[3]int{1,2,3}
-  //reading from file
-  for _,v := range element2{
-    fmt.Printf("%d\n",v)
-    
-  }
-
+	element2 := [3]int{1, 2, 3}
+	//calculating media
+	sum := 0
+	const nOfElements int = 3
+	for _, v := range element2 {
+		sum += v
+	}
+	media := float64(sum / nOfElements)
+	fmt.Printf(" la media è %2.2f\n", media)
 
 }
