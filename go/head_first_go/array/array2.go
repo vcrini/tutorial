eba6986cb0a84990e79451c0442f5da0729cd83c
vcrini <vcrini@mini.mini>
2023-01-21 14:48:05 +0100

    reading values from arguments

---
+++ go/head_first_go/array/array2.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/vcrini/x/go/array/media"
+)
+
+func main() {
+  // log.SetFlags(log.LstdFlags|log.Lshortfile)
+	arguments := os.Args[1:]
+	var elements []float64
+	for _, v := range arguments {
+		value, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			log.Fatal(err)
+			elements = nil
+		}
+		elements = append(elements, value)
+	}
+	fmt.Printf("la media dei valori di %v è %0.2f\n", elements, media.Media(elements))
+}
