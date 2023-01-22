f3873240a0bf8b4ba6c831912fd3651023ab6249
Valerio Crini <vcrini@gmail.com>
2023-01-22 23:46:40 +0100

    adding type to be "dry"

--- go/head_first_go/struct.go
+++ go/head_first_go/struct.go
@@ -3,13 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var e struct {
+	type details struct {
 		name    string
 		surname string
 		age     int
 	}
+	var e details
 	e.name = "Valerio"
 	e.surname = "Crini"
 	e.age = 48
-	fmt.Printf("person is %s %s with age %d", e.name, e.surname, e.age)
+	fmt.Printf("person is %s %s with age %d\n", e.name, e.surname, e.age)
+	var a details
+	a.name = "Adele"
+	a.surname = "Crini"
+	a.age = 8
+	fmt.Printf("person is %s %s with age %d\n", a.name, a.surname, a.age)
 }
