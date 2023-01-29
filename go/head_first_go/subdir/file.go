d3afdde451da043432ef45a9d2e8721fbcc79214
valerio <vcrini@gmail.com>
2023-01-29 17:50:37 +0100

    adding files

--- go/head_first_go/subdir/file.go
+++ go/head_first_go/subdir/file.go
@@ -1,32 +1,34 @@
 package main
+
 import (
-  "fmt"
-  "io/ioutil"
-  "log"
-  "path/filepath"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
 )
+
 func scanDirectory(path string) error {
-  fmt.Println(path)
-  files, err:=ioutil.ReadDir(path)
-  if err!=nil {
-    return err
-  }
-  for _, file:=range files {
-    filePath := filepath.Join(path, file.Name())
-    if file.IsDir() {
-      err:=scanDirectory(filePath)
-      if err != nil {
-        return err
-      }
-    } else {
-      fmt.Println(filePath)
-    }
-  }
-  return nil
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
+		if file.IsDir() {
+			err := scanDirectory(filePath)
+			if err != nil {
+				return err
+			}
+		} else {
+			fmt.Println(filePath)
+		}
+	}
+	return nil
 }
 func main() {
-  err:=scanDirectory(".")
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := scanDirectory(".")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
