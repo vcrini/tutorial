6a63a56efff5b624774d03c0671ebc1ae4c3796a
valerio <vcrini@gmail.com>
2023-01-29 18:38:01 +0100

    using a struct

--- go/head_first_go/subdir/goroutines/main.go
+++ go/head_first_go/subdir/goroutines/main.go
@@ -7,8 +7,11 @@ import (
 	"net/http"
 )
 
-
-func getLen(url string, channel chan int) {
+type Page struct {
+  URL string
+  Size int
+}
+func getLen(url string, channel chan Page) {
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -18,11 +21,11 @@ func getLen(url string, channel chan int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	channel <- len(body)
+  channel <- Page{Size: len(body), URL: url}
 
 }
 func main() {
-	sizes := make(chan int)
+	sizes := make(chan Page)
 	go getLen("https://google.com", sizes)
 	go getLen("http://vcrini.com", sizes)
 	go getLen("https://ilpost.it", sizes)
