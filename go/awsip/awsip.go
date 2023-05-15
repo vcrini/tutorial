cf0b1a4833e9479f61c298874e0050b7a46f5f1e
valerio <vcrini@gmail.com>
2023-05-15 16:20:18 +0200

    workig version

--- go/awsip/awsip.go
+++ go/awsip/awsip.go
@@ -9,23 +9,22 @@ import (
 )
 
 func main() {
-	buildCommand := "aws ec2 describe-instances --filters \"Name=tag:Name,Values=%s\" --query \"Reservations[].Instances[].{id:InstanceId,name:KeyName,ip:PrivateIpAddress}\""
+	buildCommand := "aws ec2 describe-instances --filters Name=tag:Name,Values=%s --query Reservations[].Instances[].{id:InstanceId,name:KeyName,ip:PrivateIpAddress}"
 	command := ""
 	host := flag.String("host", "bitgdi-test-ecsnode", "Name of hosts or wildcard")
 	flag.Parse()
-	switch {
-	case *host != "":
-		command = fmt.Sprintf(buildCommand, *host)
-	}
+	command = fmt.Sprintf(buildCommand, *host)
 	fmt.Println(command)
-	exe(command)
+	fmt.Println(exe(command))
 }
 
-func exe(s string) {
+func exe(s string) string {
 	splitCommand := strings.Split(s, " ")
 	cmd := exec.Command(splitCommand[0], splitCommand[1:]...)
-	if err := cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot run:\n%s\n %s", cmd, err)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot run:\n%s\n '%s'", out, err)
 		os.Exit(1)
 	}
+	return string(out)
 }
