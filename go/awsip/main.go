45f808ebece45c5fb3a7228dacd759e66d9b00ce
valerio <vcrini@gmail.com>
2023-07-19 15:11:48 +0200

    by default filtering only for 'running' instances

--- go/awsip/main.go
+++ go/awsip/main.go
@@ -10,8 +10,14 @@ import (
 
 func main() {
 	host := flag.String("host", "bitgdi-test-ecsnode", "Name of hosts or wildcard")
+	instanceStateName := flag.String("instance-state-name", "running", "The state of the instance (pending | running | shutting-down | terminated | stopping | stopped | all ).")
 	flag.Parse()
-	h := fmt.Sprintf("Name=tag:Name,Values=%s", *host)
+	var h string
+	if *instanceStateName == "all" {
+		h = fmt.Sprintf(`[{"Name": "tag:Name","Values":["%s"]}]`, *host)
+	} else {
+		h = fmt.Sprintf(`[{"Name": "tag:Name","Values":["%s"]},{"Name":"instance-state-name","Values":["%s"]}]`, *host, *instanceStateName)
+	}
 	buildCommand := []string{"aws", "ec2", "describe-instances", "--filters", h, "--query", "Reservations[].Instances[].{id:InstanceId,name:Tags[?Key == 'Name'].Value | [0],ip:PrivateIpAddress,az:Placement.AvailabilityZone}"}
 	fmt.Println(exe(buildCommand))
 }
