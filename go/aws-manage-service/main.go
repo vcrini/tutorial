49e1f03a3bad64cf546dffe62dcc3b3927ee8d2a
valerio <vcrini@gmail.com>
2024-01-05 12:09:24 +0100

    adding  show pipeline function

--- go/aws-manage-service/main.go
+++ go/aws-manage-service/main.go
@@ -15,6 +15,7 @@ func main() {
 	//cluster := flag.String("cluster", "bitgdi-test-cluster", "cluster name")
 	seconds := flag.Int("s", 0, "seconds between requests")
 	startPipelineExecution := flag.String("start-pipeline-execution", "", "Filename containing json with array of pipelines to start: e.g. [\"pipeline1\",\"pipeline2\"]")
+	showPipeline := flag.String("show-pipeline", "", "Filename containing json with array of pipelines to show: e.g. [\"pipeline1\",\"pipeline2\"]")
 	flag.Parse()
 	if *startPipelineExecution != "" {
 		s := *startPipelineExecution
@@ -33,6 +34,27 @@ func main() {
 		for _, v := range result {
 			fmt.Println(v)
 			buildCommand := []string{"aws", "codepipeline", "start-pipeline-execution", "--name", v.(string)}
+			fmt.Println(utils.Exe(buildCommand))
+			time.Sleep(time.Duration(*seconds) * time.Second)
+		}
+	}
+	if *showPipeline != "" {
+		s := *showPipeline
+		byteValue, err := readJson(s)
+		if err != nil {
+			fmt.Println("could not parse json file")
+			os.Exit(1)
+		}
+		var result []interface{}
+		err = json.Unmarshal([]byte(byteValue), &result)
+		fmt.Println(result)
+		if err != nil {
+			fmt.Printf("can't unmarshall json: %s", err.Error())
+			os.Exit(2)
+		}
+		for _, v := range result {
+			fmt.Println(v)
+			buildCommand := []string{"aws", "codepipeline", "list-action-executions", "--pipeline-name", v.(string), "--query", "actionExecutionDetails[0].{status: status, stageName: stageName, startTime: startTime, lastUpdateTime: lastUpdateTime}"}
 			fmt.Println(utils.Exe(buildCommand))
 			time.Sleep(time.Duration(*seconds) * time.Second)
 		}
