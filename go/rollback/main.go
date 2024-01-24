1cebd6e830f57a595ee33196c48c4aa4ea98c5ac
valerio <vcrini@gmail.com>
2024-01-24 17:33:07 +0100

    added rudimentary local invocation

--- go/rollback/main.go
+++ go/rollback/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"github.com/vcrini/go-utils"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/versions", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello, present versions are"))
+		buildCommand := []string{"/Users/vcrini/go/bin/aws-manager-service", "--findVersion", "dpl-app-appdemo-backend"}
+		out := utils.Exe(buildCommand)
+		w.Write([]byte(out))
+	})
 	http.ListenAndServe(":8080", nil)
-
 }
