dfce565067001b1ec28f4bce669f292428f5ab9c
valerio <vcrini@gmail.com>
2024-02-02 16:43:05 +0100

    refactored code to being able to use global and local params

--- go/aws-manager-service/functions.go
+++ go/aws-manager-service/functions.go
@@ -3,13 +3,16 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"github.com/vcrini/go-utils"
 	"io"
+	"log"
 	"os"
 	"regexp"
 	"sort"
 	"strings"
+	"time"
 )
 
 func readJson(fileName string) ([]byte, error) {
@@ -36,7 +39,17 @@ func deployOrRollback(service string, cluster string, version int) {
 	fmt.Println(utils.Exe(buildCommand))
 
 }
-func findVersionMax(service string) {
+func findVersionMax(args []string) {
+	flag := flag.NewFlagSet("find-version", flag.ExitOnError)
+	registerGlobalFlags(flag)
+	err := flag.Parse(args)
+	if err != nil {
+		log.Fatalf("error in parsing params %s", args)
+	}
+	if len(args) == 0 {
+		log.Fatal(" you must provide a filename containing json with array of pipelines to start: e.g. [\"pipeline1\",\"pipeline2\"] triggers specified pipelines")
+	}
+	service := args[0]
 	// find oldest version available
 	buildCommand := []string{"aws", "ecr", "list-images", "--repository-name", fmt.Sprintf("%s-snapshot", service), "--query", "imageIds[-1].imageTag", "--output", "text"}
 	versionOldestSnapshot := strings.TrimSuffix(utils.Exe(buildCommand), "\n")
@@ -56,7 +69,7 @@ func findVersionMax(service string) {
 	//     "arn:aws:ecs:eu-west-1:796341525871:task-definition/dpl-app-appdemo-backend:1"
 	//
 	var result []interface{}
-	var err = json.Unmarshal([]byte(out), &result)
+	err = json.Unmarshal([]byte(out), &result)
 	if err != nil {
 		fmt.Printf("can't unmarshall json: %s", err.Error())
 		os.Exit(2)
@@ -88,7 +101,141 @@ func findVersionMax(service string) {
 	fmt.Print(string(u))
 
 }
-func startOrStop(file string, cluster string, action string) {
+
+func listServices(args []string) {
+	flag := flag.NewFlagSet("list-services", flag.ExitOnError)
+	var (
+		cluster = flag.String("cluster", "", "cluster where to operate")
+	)
+	registerGlobalFlags(flag)
+	err := flag.Parse(args)
+	if err != nil {
+		log.Fatalf("error in parsing params %s", args)
+	}
+	services := make(map[string]int)
+	if *cluster == "" {
+		log.Fatal("parameter 'cluster' is mandatory")
+	}
+	buildCommand := []string{"aws", "ecs", "list-services", "--cluster", *cluster, "--query", "serviceArns[*]"}
+	var out = utils.Exe(buildCommand)
+	var result []interface{}
+	err = json.Unmarshal([]byte(out), &result)
+	if err != nil {
+		log.Fatalf("can't unmarshall json: %s", err.Error())
+	}
+	r, _ := regexp.Compile("([^/]+)$")
+	// to avoid too many requests
+	for _, chunk := range chunkBy(result, 10) {
+		var names []string
+		for _, v := range chunk {
+			var name = r.FindString(v.(string))
+			names = append(names, name)
+		}
+		buildCommand := []string{"aws", "ecs", "describe-services", "--cluster", *cluster, "--services"}
+		buildCommand = append(buildCommand, names...)
+		buildCommand = append(buildCommand, []string{"--query", "services[*].{serviceName: serviceName, desiredCount: desiredCount}"}...)
+		out = utils.Exe(buildCommand)
+		var result2 []interface{}
+		var err = json.Unmarshal([]byte(out), &result2)
+		if err != nil {
+			log.Fatalf("can't unmarshall json: %s", err.Error())
+		}
+		for _, x := range result2 {
+			var k = x.(map[string]interface{})["serviceName"].(string)
+			var v = int(x.(map[string]interface{})["desiredCount"].(float64))
+			services[k] = v
+		}
+		time.Sleep(time.Duration(*_seconds) * time.Second)
+	}
+	u, err := json.Marshal(services)
+	if err != nil {
+		log.Fatalf("can't marshall services: %s", err.Error())
+	}
+	fmt.Print(string(u))
+}
+func showPipeline(args []string) {
+	flag := flag.NewFlagSet("show-pipeline", flag.ExitOnError)
+	registerGlobalFlags(flag)
+	err := flag.Parse(args)
+	if err != nil {
+		log.Fatalf("error in parsing params %s", args)
+	}
+	if len(args) == 0 {
+		log.Fatal(" you must provide as show-pipeline argument a file with a json list of pipelines: e.g. [\"pipeline1\",\"pipeline2\"]")
+	}
+	s := args[0]
+	byteValue, err := readJson(s)
+	if err != nil {
+		fmt.Println("could not parse json file")
+		os.Exit(1)
+	}
+	var result []interface{}
+	err = json.Unmarshal([]byte(byteValue), &result)
+	if err != nil {
+		fmt.Printf("can't unmarshall json: %s", err.Error())
+		os.Exit(2)
+	}
+	for _, v := range result {
+		fmt.Println(v)
+		buildCommand := []string{"aws", "codepipeline", "list-action-executions", "--pipeline-name", v.(string), "--query", "actionExecutionDetails[0].{status: status, stageName: stageName, startTime: startTime, lastUpdateTime: lastUpdateTime}"}
+		fmt.Println(utils.Exe(buildCommand))
+		time.Sleep(time.Duration(*_seconds) * time.Second)
+	}
+}
+func startPipelineExecution(args []string) {
+	flag := flag.NewFlagSet("start-pipeline-execution", flag.ExitOnError)
+	registerGlobalFlags(flag)
+	err := flag.Parse(args)
+	if err != nil {
+		log.Fatalf("error in parsing params %s", args)
+	}
+	// works only if params are before args
+	args = flag.Args()
+	if len(args) == 0 {
+		log.Fatal(" you must provide a filename containing json with array of pipelines to start: e.g. [\"pipeline1\",\"pipeline2\"] triggers specified pipelines")
+	}
+	s := args[0]
+	byteValue, err := readJson(s)
+	if err != nil {
+		fmt.Println("could not parse json file")
+		os.Exit(1)
+	}
+	var result []interface{}
+	err = json.Unmarshal([]byte(byteValue), &result)
+	if err != nil {
+		fmt.Printf("can't unmarshall json: %s", err.Error())
+		os.Exit(2)
+	}
+	for _, v := range result {
+		fmt.Println(v)
+		buildCommand := []string{"aws", "codepipeline", "start-pipeline-execution", "--name", v.(string)}
+		fmt.Println(utils.Exe(buildCommand))
+		time.Sleep(time.Duration(*_seconds) * time.Second)
+	}
+}
+func startOrStop(action string, args []string) {
+	flag := flag.NewFlagSet("startOrStop", flag.ExitOnError)
+	var (
+		cluster = flag.String("cluster", "", "cluster where to operate")
+	)
+	registerGlobalFlags(flag)
+	err := flag.Parse(args)
+	if err != nil {
+		log.Fatalf("error in parsing params %s", args)
+	}
+	// works only if params are before args
+	args = flag.Args()
+	if *cluster == "" {
+		log.Fatal("parameter 'cluster' is mandatory")
+	}
+	if len(args) == 0 {
+		if action == "start" {
+			log.Fatal("you must provide as an argument a file with a json dictionary where keys are services to start if value is '0' or higher")
+		} else {
+			log.Fatal("you must provide as an argument a  file with a json dictionary where keys are services to where keys are services to stop")
+		}
+	}
+	file := args[0]
 	byteValue, err := readJson(file)
 	if err != nil {
 		fmt.Println("could not parse json file")
@@ -109,8 +256,38 @@ func startOrStop(file string, cluster string, action string) {
 			count = int(v.(float64))
 		}
 		fmt.Printf("service: %s is being set to: %s\n", k, action)
-		buildCommand := []string{"aws", "ecs", "update-service", "--cluster", cluster, "--service", k, "--desired-count", fmt.Sprint(count), "--query", "service.{desiredCount: desiredCount}"}
+		buildCommand := []string{"aws", "ecs", "update-service", "--cluster", *cluster, "--service", k, "--desired-count", fmt.Sprint(count), "--query", "service.{desiredCount: desiredCount}"}
 		fmt.Println(utils.Exe(buildCommand))
 	}
 
+}
+func rollback(args []string) {
+	flag := flag.NewFlagSet("rollback", flag.ExitOnError)
+	var (
+		cluster = flag.String("cluster", "", "cluster where to operate")
+		version = flag.Int("version", -1, "version to rollback")
+		service = flag.String("service", "", "service to be rollbacked")
+	)
+	registerGlobalFlags(flag)
+	err := flag.Parse(args)
+	if err != nil {
+		log.Fatalf("error in parsing params %s", args)
+	}
+	//args = flag.Args()
+	if *service == "" {
+		log.Fatal("parameter 'service' is mandatory")
+	}
+	if *cluster == "" {
+		log.Fatal("parameter 'cluster' is mandatory")
+	}
+	if *version < 0 {
+
+		fmt.Println("parameter 'version' is mandatory and must be >0")
+	}
+	deployOrRollback(*service, *cluster, *version)
+}
+func registerGlobalFlags(fset *flag.FlagSet) {
+	flag.VisitAll(func(f *flag.Flag) {
+		fset.Var(f.Value, f.Name, f.Usage)
+	})
 }
--- go/aws-manager-service/main.go
+++ go/aws-manager-service/main.go
@@ -1,128 +1,38 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"github.com/vcrini/go-utils"
-	"os"
-	"regexp"
-	"time"
+	"log"
+)
+
+var (
+	_seconds = flag.Int("s", 0, "seconds between requests")
 )
 
 func main() {
-	cluster := flag.String("cluster", "", "cluster where to operate, used with 'list-services', 'stop-services', 'start-services'")
-	listServices := flag.Bool("list-services", false, "return services with desired tasks {\"pipeline1\": 1, \"pipeline2\": 0]")
-	rollback := flag.String("rollback", "", "service to be rollbacked")
-	seconds := flag.Int("s", 0, "seconds between requests")
-	showPipeline := flag.String("show-pipeline", "", "show pipeline status")
-	startPipelineExecution := flag.String("start-pipeline-execution", "", "given filename containing json with array of pipelines to start: e.g. [\"pipeline1\",\"pipeline2\"] triggers specified pipelines")
-	startService := flag.String("start-services", "", "reads a json dictionary where keys are services to start if value is '0' or higher")
-	stopService := flag.String("stop-services", "", "reads a json dictionary where keys are services to stop")
-	version := flag.Int("version", -1, "version to rollback")
-	findVersion := flag.String("findVersion", "", "name of service you want to find version")
 	flag.Parse()
-	if *startPipelineExecution != "" {
-		s := *startPipelineExecution
-		byteValue, err := readJson(s)
-		if err != nil {
-			fmt.Println("could not parse json file")
-			os.Exit(1)
-		}
-		var result []interface{}
-		err = json.Unmarshal([]byte(byteValue), &result)
-		if err != nil {
-			fmt.Printf("can't unmarshall json: %s", err.Error())
-			os.Exit(2)
-		}
-		for _, v := range result {
-			fmt.Println(v)
-			buildCommand := []string{"aws", "codepipeline", "start-pipeline-execution", "--name", v.(string)}
-			fmt.Println(utils.Exe(buildCommand))
-			time.Sleep(time.Duration(*seconds) * time.Second)
-		}
-	} else if *showPipeline != "" {
-		s := *showPipeline
-		byteValue, err := readJson(s)
-		if err != nil {
-			fmt.Println("could not parse json file")
-			os.Exit(1)
-		}
-		var result []interface{}
-		err = json.Unmarshal([]byte(byteValue), &result)
-		if err != nil {
-			fmt.Printf("can't unmarshall json: %s", err.Error())
-			os.Exit(2)
-		}
-		for _, v := range result {
-			fmt.Println(v)
-			buildCommand := []string{"aws", "codepipeline", "list-action-executions", "--pipeline-name", v.(string), "--query", "actionExecutionDetails[0].{status: status, stageName: stageName, startTime: startTime, lastUpdateTime: lastUpdateTime}"}
-			fmt.Println(utils.Exe(buildCommand))
-			time.Sleep(time.Duration(*seconds) * time.Second)
-		}
-	} else if *listServices {
-		services := make(map[string]int)
-		if *cluster == "" {
-			fmt.Println("parameter 'cluster' is mandatory")
-			os.Exit(4)
-
-		}
-		buildCommand := []string{"aws", "ecs", "list-services", "--cluster", *cluster, "--query", "serviceArns[*]"}
-		var out = utils.Exe(buildCommand)
-		var result []interface{}
-		var err = json.Unmarshal([]byte(out), &result)
-		if err != nil {
-			fmt.Printf("can't unmarshall json: %s", err.Error())
-			os.Exit(2)
-		}
-		r, _ := regexp.Compile("([^/]+)$")
-		// to avoid too many requests
-		for _, chunk := range chunkBy(result, 10) {
-			var names []string
-			for _, v := range chunk {
-				var name = r.FindString(v.(string))
-				names = append(names, name)
-			}
-			buildCommand := []string{"aws", "ecs", "describe-services", "--cluster", *cluster, "--services"}
-			buildCommand = append(buildCommand, names...)
-			buildCommand = append(buildCommand, []string{"--query", "services[*].{serviceName: serviceName, desiredCount: desiredCount}"}...)
-			out = utils.Exe(buildCommand)
-			var result2 []interface{}
-			var err = json.Unmarshal([]byte(out), &result2)
-			if err != nil {
-				fmt.Printf("can't unmarshall json: %s", err.Error())
-				os.Exit(2)
-			}
-			for _, x := range result2 {
-				var k = x.(map[string]interface{})["serviceName"].(string)
-				var v = int(x.(map[string]interface{})["desiredCount"].(float64))
-				services[k] = v
-			}
-			time.Sleep(time.Duration(*seconds) * time.Second)
-		}
-		u, err := json.Marshal(services)
-		if err != nil {
-			fmt.Printf("can't marshall services: %s", err.Error())
-			os.Exit(3)
-		}
-		fmt.Print(string(u))
-	} else if *startService != "" {
-		startOrStop(*startService, *cluster, "start")
-	} else if *stopService != "" {
-		startOrStop(*stopService, *cluster, "stop")
-	} else if *rollback != "" {
-		if *cluster == "" {
-			fmt.Println("parameter 'cluster' is mandatory")
-			os.Exit(4)
-		}
-		if *version < 0 {
-
-			fmt.Println("parameter 'version' is mandatory and must be >0")
-		}
-		deployOrRollback(*rollback, *cluster, *version)
-	} else if *findVersion != "" {
-		findVersionMax(*findVersion)
-	} else {
-		fmt.Println("Please use: -h for launch details ")
+	args := flag.Args()
+	if len(args) == 0 {
+		log.Fatal("Please specify a subcommand.")
+	}
+	cmd, args := args[0], args[1:]
+	switch cmd {
+	case "list-services":
+		listServices(args)
+	case "rollback":
+		rollback(args)
+	case "show-pipeline":
+		showPipeline(args)
+	case "start-services":
+		startOrStop("start", args)
+	case "stop-services":
+		startOrStop("stop", args)
+	case "start-pipeline-execution":
+		startPipelineExecution(args)
+	case "find-version":
+		findVersionMax(args)
+	default:
+		log.Fatalf("Unrecognized command %q. "+
+			"Command must be one of: find-version, list-services, show-pipeline, start-pipeline-execution, start-services, stop-services, rollback ...", cmd)
 	}
 }
