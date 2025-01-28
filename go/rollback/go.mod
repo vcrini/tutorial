759df1b4fe551f05afc4cd57b7d25561b3b2a135
valerio <vcrini@gmail.com>
2025-01-28 17:18:42 +0100

    new change

--- go/rollback/go.mod
+++ go/rollback/go.mod
@@ -2,4 +2,8 @@ module vcrini.com/rollback
 
 go 1.21.6
 
-require github.com/vcrini/go-utils v0.2.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/vcrini/go-utils v0.2.0
+)
--- go/rollback/main.go
+++ go/rollback/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"net/http"
 
-	"github.com/vcrini/go-utils"
+	// "github.com/vcrini/go-utils"
+	"vcrini.com/rollback/api"
 )
 
 func main() {
-	http.HandleFunc("/versions", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello, present versions are:\n"))
-		buildCommand := []string{"/Users/vcrini/go/bin/aws-manager-service", "--findVersion", "dpl-app-appdemo-backend"}
-		out := utils.Exe(buildCommand)
-		w.Write([]byte(out))
-	})
-	http.ListenAndServe(":8080", nil)
+	// http.HandleFunc("/versions", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("hello, present versions are:\n"))
+	// 	buildCommand := []string{"/Users/vcrini/go/bin/aws-manager-service", "--findVersion", "dpl-app-appdemo-backend"}
+	// 	out := utils.Exe(buildCommand)
+	// 	w.Write([]byte(out))
+	// })
+	// http.ListenAndServe(":8080", nil)
+	srv := api.NewServer()
+	//srv := api.NewServer()
+	http.ListenAndServe(":8080", srv)
 }
