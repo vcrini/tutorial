87378435249a4f4e5d0ace63098d13da17425b56
valerio <vcrini@gmail.com>
2025-11-04 12:43:57 +0100

    feat: adding statemachine example

---
+++ python/python_server_machine/example.py
@@ -0,0 +1,33 @@
+from statemachine import State, StateMachine
+
+
+class TrafficLightMachine(StateMachine):
+    "A traffic light machine"
+    green = State(initial=True)
+    yellow = State()
+    red = State()
+
+    cycle = (
+        green.to(yellow)
+        | yellow.to(red)
+        | red.to(green)
+    )
+
+    def before_cycle(self, event: str, source: State, target: State, message: str = ""):
+        message=". " + message if message else ""
+        return f"Running {event} from {source.id} to {target.id}{message}"
+
+    def on_enter_red(self):
+        print("Don't move.")
+
+    def on_exit_red(self):
+        print("Go ahead!")
+if __name__=="__main__":
+    sm = TrafficLightMachine()
+    print(sm.current_state)
+    print(sm.send("cycle"))
+    print(sm.current_state.id)
+    print(sm.send("cycle"))
+    print(sm.send("cycle"))
+    print(sm.send("cycle"))
+
