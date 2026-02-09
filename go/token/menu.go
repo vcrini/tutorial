8304716ed4a12abd67911b2b4098553947b7262a
valerio <vcrini@gmail.com>
2026-02-09 22:34:23 +0100

    feat: removing unusued commands

--- go/token/menu.go
+++ go/token/menu.go
@@ -9,8 +9,6 @@ import (
 
 var menuChoices = []string{
 	"Crea PNG",
-	"Ricarica PNG da disco",
-	"Salva PNG su disco",
 	"Resetta Tutti i Token PNG",
 	"Esci",
 }
@@ -22,45 +20,6 @@ func (m model) handleMenuChoice(choice string) (model, tea.Cmd) {
 		m.textInput.Reset()
 		m.message = "Inserisci il nome del nuovo PNG:"
 		return m, textinput.Blink
-	case "Ricarica PNG da disco":
-		selectedName := ""
-		if m.selectedPNGIndex >= 0 && m.selectedPNGIndex < len(m.pngs) {
-			selectedName = m.pngs[m.selectedPNGIndex].Name
-		}
-		pngs, selected, err := loadPNGList(dataFile)
-		if err != nil {
-			m.message = fmt.Sprintf("Errore nel caricare %s: %v", dataFile, err)
-		} else {
-			m.pngs = pngs
-			m.selectedPNGIndex = -1
-			if len(m.pngs) > 0 {
-				if selected != "" {
-					for i, p := range m.pngs {
-						if p.Name == selected {
-							m.selectedPNGIndex = i
-							break
-						}
-					}
-				} else {
-					for i, p := range m.pngs {
-						if p.Name == selectedName && selectedName != "" {
-							m.selectedPNGIndex = i
-							break
-						}
-					}
-				}
-				if m.selectedPNGIndex == -1 {
-					m.selectedPNGIndex = 0
-				}
-			}
-			m.message = fmt.Sprintf("Lista PNG caricata da %s (%d).", dataFile, len(m.pngs))
-		}
-	case "Salva PNG su disco":
-		if err := savePNGList(dataFile, m.pngs, selectedPNGName(m.pngs, m.selectedPNGIndex)); err != nil {
-			m.message = fmt.Sprintf("Errore nel salvataggio su %s: %v", dataFile, err)
-		} else {
-			m.message = fmt.Sprintf("Lista PNG salvata su %s.", dataFile)
-		}
 	case "Resetta Tutti i Token PNG":
 		if len(m.pngs) == 0 {
 			m.message = "Nessun PNG presente per resettare i token."
